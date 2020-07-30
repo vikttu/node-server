aa8540a618a66cb943214731b228bacdb636de24
vikttu <nespolopeixoto@outlook.com>
2020-07-30 16:25:13 -0300

    making a simple template

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const port = 3000;
 const app = express();
+const path = require('path');
+
+//setup view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res, next) => {
-    res.send({
+    res.render('index', {
         title: "My first Express server",
         version: "0.0.0"
     });
 });
+
+app.get('/resume', (req, res, next) => {
+    res.render('resume',  {
+        title: 'My resume',
+        name: 'Viktor Nespolo Peixoto',
+        profession: 'Software Developer',
+        description: 'Repudiandae eaque voluptate enim corporis impedit. Dignissimos eum est dolor qui qui omnis et consequatur.',
+        experience: [{
+            company: 'Lorem Ipsum',
+            office: 'Dolor Sit',
+            description: 'Saepe a ducimus et omnis non. Officia tenetur deleniti voluptatem sint.'
+        },
+        {
+            company: 'Amet',
+            office: 'Id itaque',
+            description: 'Saepe a ducimus et omnis non. Officia tenetur deleniti voluptatem sint.'
+        }],
+        education: [{
+            institution: 'Vida',
+            description: 'Sobrevivência nas ruas'
+        }],
+        skills: ['backend', 'frontend', 'infra', 'mobile']
+    });
+})
 
 app.listen(port, err => {
     console.log(`Server is listening on port ${port}`);
